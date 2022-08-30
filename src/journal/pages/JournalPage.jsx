72261896c2b0dbbdc37d479899acde84aa9e4dfb
Alexander <alexander.martinez@lapatota.cl>
2022-08-29 20:02:48 -0400

    NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eos beatae id fuga deleniti sunt eveniet nulla, maiores voluptatem dicta asperiores architecto culpa? Itaque laboriosam excepturi tempore est nihil aut.</Typography > */}
 
-      <NothingSelectedView />
-      {/* NoteView */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
