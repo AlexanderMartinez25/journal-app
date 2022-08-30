ec60379623860c205c09788cdfe939fe2ca495bc
Alexander <alexander.martinez@lapatota.cl>
2022-08-29 20:30:15 -0400

    Boton Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 
@@ -7,8 +8,22 @@ export const JournalPage = () => {
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae eos beatae id fuga deleniti sunt eveniet nulla, maiores voluptatem dicta asperiores architecto culpa? Itaque laboriosam excepturi tempore est nihil aut.</Typography > */}
 
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
