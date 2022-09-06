d1718ae7d9c09e635fccde8e87ddd9e36d8804f3
Alexander <alexander.martinez@lapatota.cl>
2022-09-05 21:14:21 -0400

    Manejo del formulario de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,36 @@
 import { Link as RouterLink } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import { Google } from "@mui/icons-material"
 import { Grid, Typography, TextField, Button, Link } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks/useForm'
+import { chekingAutentication, checkingCredentials } from '../../store/auth'
 
 export const LoginPage = () => {
+
+
+  const dispatch = useDispatch();
+
+  const { email, password, onInputChange } = useForm({
+    email: 'amartinez.byob@gmail.com',
+    password: '123456'
+  })
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    dispatch(chekingAutentication())
+    console.log({ email, password });
+  }
+
+  const onGoogleSingIn = () => {
+    console.log('onGoogleSingIn');
+    dispatch(checkingCredentials());
+  }
+
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +38,8 @@ export const LoginPage = () => {
               type="email"
               placeholder="correo@google.com"
               fullWidth
+              name='email'
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -23,18 +49,23 @@ export const LoginPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name='password'
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Login
               </Button>
             </Grid>
 
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button
+                variant="contained"
+                fullWidth
+                onClick={onGoogleSingIn}>
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
---
+++ src/store/auth/thuks.js
@@ -0,0 +1,13 @@
+import { checkingCredentials } from "./";
+
+export const chekingAutentication = (email, password) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
+
+export const startGoogleSingIn = () => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
