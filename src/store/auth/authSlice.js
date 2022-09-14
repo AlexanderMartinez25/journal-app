4bb1156144578bc742ed6b36f62e5c83a95527fe
Alexander <alexander.martinez@lapatota.cl>
2022-09-14 00:17:20 -0300

    Mantener el estado de la autenticación al recargar

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -12,7 +12,7 @@ export const authSlice = createSlice({
   },
   reducers: {
     login: (state, { payload }) => {
-      state.status = "autenticated";
+      state.status = "authenticated";
       state.uid = payload.uid;
       state.email = payload.email;
       state.displayName = payload.displayName;
@@ -21,7 +21,7 @@ export const authSlice = createSlice({
     },
 
     logout: (state, { payload }) => {
-      state.status = "not-autenticated";
+      state.status = "not-authenticated";
       state.uid = null;
       state.email = null;
       state.displayName = null;
