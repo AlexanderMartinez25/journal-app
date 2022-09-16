28a2376c81cd6b615a55d26c6eb10432182b25ac
Alexander <alexander.martinez@lapatota.cl>
2022-09-16 18:39:52 -0300

    Cargar notas de Firestore

--- src/store/journal/thuks.js
+++ src/store/journal/thuks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, setActiveNote, savingNewNote } from "./";
+import { addNewEmptyNote, setActiveNote, savingNewNote, setNotes } from "./";
+import { loadNotes } from "../../helpers";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -23,6 +24,15 @@ export const startNewNote = () => {
     // dispatch
     dispatch(addNewEmptyNote(newNote));
     dispatch(setActiveNote(newNote));
-    // dispatch(activarNote)
+  };
+};
+
+export const startLoadingNotes = () => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+    if (!uid) throw new Error("El UID del usuario no existe");
+    const notes = await loadNotes(uid);
+
+    dispatch(setNotes(notes));
   };
 };
