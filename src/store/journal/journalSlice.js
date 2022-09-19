db2cd51a5450332b467c0f2a60df0a82174af51d
Alexander <alexander.martinez@lapatota.cl>
2022-09-19 18:36:58 -0300

    documentación final

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -58,6 +58,13 @@ export const journalSlice = createSlice({
     deleteNoteById: (state, action) => {
       state.active = null;
       state.notes = state.notes.filter((note) => note.id !== action.payload);
+
+      // si no fuera por redux-toolkit
+      // return {
+      //   ...state,
+      //   active: null,
+      //   notes: state.notes.filter((note) => note.id !== action.payload),
+      // };
     },
   },
 });
