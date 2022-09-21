cd9d54d78b1c40b0e5975816eaed69892d07d682
Alexander <alexander.martinez@lapatota.cl>
2022-09-20 21:32:53 -0300

    Thunks - checkingCredentials correcto e incorrecto

--- tests/store/auth/thuks.test.js
+++ tests/store/auth/thuks.test.js
@@ -1,5 +1,10 @@
-import { checkingCredentials } from "../../../src/store/auth";
-import { chekingAutentication } from "../../../src/store/auth/thuks";
+import { signInWidthGoolge } from "../../../src/firebase/provider";
+import { checkingCredentials, login, logout } from "../../../src/store/auth";
+import {
+  chekingAutentication,
+  startGoogleSingIn,
+} from "../../../src/store/auth/thuks";
+import { demoUser } from "../../fixtures/authFixtures";
 
 jest.mock("../../../src/firebase/provider");
 
@@ -11,5 +16,27 @@ describe("Pruebas en AuthThunks", () => {
   test("debe de invocar el chekingCredentials", async () => {
     await chekingAutentication()(dispatch);
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+  });
+
+  test("startGoogleSingIn debe de llamar chekingCredentials y login - Exito", async () => {
+    const loginData = { ok: true, ...demoUser };
+    await signInWidthGoolge.mockResolvedValue(loginData);
+
+    //thunk
+    await startGoogleSingIn()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(login(loginData));
+  });
+
+  test("startGoogleSingIn debe de llamar chekingCredentials y logout - Error", async () => {
+    const loginData = { ok: false, errorMessage: "Un error en Google" };
+    await signInWidthGoolge.mockResolvedValue(loginData);
+
+    //thunk
+    await startGoogleSingIn()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage));
   });
 });
