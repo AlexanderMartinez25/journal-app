42a4bcbe8561857ceb51caf1405ae4be6fd952e8
Alexander <alexander.martinez@lapatota.cl>
2022-09-20 23:23:00 -0300

    Prueba completa sobre inserción

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../../src/firebase/config";
 import { startNewNote } from "../../../src/store/journal";
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+} from "../../../src/store/journal/journalSlice";
 
 describe("Pruebas en Journal Thunks", () => {
   const dispatch = jest.fn();
@@ -11,5 +18,31 @@ describe("Pruebas en Journal Thunks", () => {
     getState.mockReturnValue({ auth: { uid } });
 
     await startNewNote()(dispatch, getState);
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        body: "",
+        date: expect.any(Number),
+        id: expect.any(String),
+        title: "",
+      })
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        body: "",
+        date: expect.any(Number),
+        id: expect.any(String),
+        title: "",
+      })
+    );
+
+    //Borrar de firebase
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+    const docs = await getDocs(collectionRef);
+
+    const deletePromises = [];
+    docs.forEach((doc) => deletePromises.push(deleteDoc(doc.ref)));
+
+    await Promise.all(deletePromises);
   });
 });
