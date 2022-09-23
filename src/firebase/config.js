c78e1b9d1de9743e6b816b35669dc6b7c071a642
Alexander <alexander.martinez@lapatota.cl>
2022-09-22 21:32:54 -0300

    Configurar variables de entorno de Firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
-import { getEnviroments } from "../helpers";
+import { getEnviroments } from "../helpers/getEnviroments";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,21 +17,37 @@ import { getEnviroments } from "../helpers";
 //   appId: "1:938292154947:web:a83cd75d1cd851c488faf6",
 // };
 
-const env = getEnviroments();
-console.log(env);
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnviroments();
 
 // console.log(process.env);
 // console.log(import.meta.env);
 
 //Testing
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDYldhNxYAd5T0ZvZzR-xtMdcXPSw5QTSk",
+//   authDomain: "test-react-211fa.firebaseapp.com",
+//   projectId: "test-react-211fa",
+//   storageBucket: "test-react-211fa.appspot.com",
+//   messagingSenderId: "584703150276",
+//   appId: "1:584703150276:web:17952f354d8c2c976e2191",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDYldhNxYAd5T0ZvZzR-xtMdcXPSw5QTSk",
-  authDomain: "test-react-211fa.firebaseapp.com",
-  projectId: "test-react-211fa",
-  storageBucket: "test-react-211fa.appspot.com",
-  messagingSenderId: "584703150276",
-  appId: "1:584703150276:web:17952f354d8c2c976e2191",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
+console.log(firebaseConfig);
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
